ba28a9946683fade43a6b1ab945ded3614f970a9
Idris <ilakehal31@gmail.com>
2024-06-04 16:10:01 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: [process.env.NUXT_UI_PRO_PATH || '@nuxt/ui-pro'],
+  modules: ['@nuxt/ui', '@nuxt/fonts', '@vueuse/nuxt', "@nuxthub/core"],
+  ui: {
+    icons: ['heroicons', 'simple-icons'],
+    safelistColors: ['primary', 'red', 'orange', 'green']
+  },
+  devtools: {
+    enabled: true
+  }
+})
